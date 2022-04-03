a60928719d1a32ebcd941325a764be8f79b30960
PyBaker <freddygekko3@gmail.com>
2022-04-03 22:25:57 +0300

    done with the tricky regex one ... will optimise it with time as I leanrn more about regex :)

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
-re = //
+
+SENDER = /(?<=from:)(\+?\d+)(?=\])/
+RECEIVER = /(?<=to:)(\+?\d+)(?=\])/
+FLAGS = /(?<=flags:)(\+?\S+)(?=\])/
+
 ARGV.each do |a|
-  puts a.scan(re)
+  #data = a.scan(SENDER)  + "," + a.scan(RECEIVER)   + "," + a.scan(FLAGS) + "\n"
+
+  #puts a.scan(SENDER).to_s  + "," + a.scan(RECEIVER).to_s   + "," + a.scan(FLAGS).to_s
+
+  #data = a.scan(SENDER).to_s  + "," + a.scan(RECEIVER).to_s
+  print a.scan(SENDER)[0][0]
+  print ','
+  print a.scan(RECEIVER)[0][0]
+  print ','
+  print a.scan(FLAGS)[0][0]
+  puts
 end
