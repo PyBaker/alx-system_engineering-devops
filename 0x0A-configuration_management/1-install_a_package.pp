1a60f28df12e2541fb485b920d12cfa8a5b19855
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-07-27 19:00:56 +0200

    hotfix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # script for installing lint
 
-package { 'puppet-lint':
-  ensure   => '2.5.2',
-  provider => 'gem',
-}
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pep3',
 }
