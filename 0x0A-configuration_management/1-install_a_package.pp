a02fc21dc4ce7d77a7d7bc2980e046765836e2c2
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-07-27 19:04:46 +0200

    hotfix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # script for installing lint
-
-package { 'flask':
+include python
+include python::flask
+package { 'Flask 2.1.0':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  name     => 'flask',
+  provider => 'pip3'
+  require  => Class['python::flask'],
 }
