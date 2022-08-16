6dc06546407b4ca3d5e39fc75d4cd629e537a03a
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-08-16 20:38:24 +0200

    hotfix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,7 @@ exec {"Comz":
 command => 'sudo apt install python3;python3 -m pip install "flask==2.1.0"',
 path    => '/usr/local/bin/:/bin/',
 }
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
+}
