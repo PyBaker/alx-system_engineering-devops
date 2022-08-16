db5478b3acde7c10f37679ac796fa549ea1b93d9
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-08-16 21:14:11 +0200

    hotfix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # script for installing puppet-lint
 
+exec {"Comz":
+command => 'sudo apt install puppet',
+path    => '/usr/local/bin/:/bin/',
+}
 package { 'flask':
-ensure   => 'installed',
+ensure   => '2.1.0',
 provider => 'pip',
 }
